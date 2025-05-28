c421b508e4b0b1c55d9a1571e82f27ae3c3ba7d0
Renato682 <renatoarouche3@gmail.com>
2025-05-28 18:14:14 -0300

    Create text.php

---
+++ text.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <h1>
+        O que é o autismo?
+    </h1>
+    <h3>
+        O Transtorno do Espectro Autista (TEA) é uma condição do neurodesenvolvimento que afeta a forma como a pessoa se comunica, interage socialmente e percebe o mundo ao seu redor. Cada pessoa dentro do espectro é única  com suas próprias habilidades, 
+        desafios, formas de se expressar e de se conectar. 
+    </h3>
+</body>
+</html>
