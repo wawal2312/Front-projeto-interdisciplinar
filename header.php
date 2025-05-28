a57b6d063a9e44c1dd5e9423417fbb8119947686
Renato682 <renatoarouche3@gmail.com>
2025-05-28 18:13:11 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="projeto.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="input-group">
+    <div class="input-group-text" id="btnGroupAddon">Search</div>
+    <input type="text" class="form-control" placeholder="Input group example" aria-label="Input group example" aria-describedby="btnGroupAddon">
+    </div>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+   <button type="button" class="btn btn-link">Home</button>
+   <button type="button" class="btn btn-link">O que é o autismo?</button>
+   <button type="button" class="btn btn-link">Quem somos?</button>
+   <button type="button" class="btn btn-link">Nosso trabalho</button>
+   </div>
+</body>
+</html>
