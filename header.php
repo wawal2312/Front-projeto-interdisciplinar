9cbf767b545fcb8df886912a8ef5bb0fabc68b0e
Renato682 <renatoarouche3@gmail.com>
2025-05-29 16:21:01 -0300

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="projeto.css">
-    <title>Document</title>
+    <title>Espectro</title>
     <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
 </head>
