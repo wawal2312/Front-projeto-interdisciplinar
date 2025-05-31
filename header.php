cea4e0017b633bb38b6493ba127765310fe73c02
Renato682 <renatoarouche3@gmail.com>
2025-05-30 22:42:34 -0300

    Update header.php

--- header.php
+++ header.php
@@ -4,20 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="projeto.css">
-    <title>Espectro</title>
+    <title>Document</title>
     <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="input-group">
-    <div class="input-group-text" id="btnGroupAddon">Search</div>
+    <div class="input-group-text" id="btnGroupAddon">Pesquisar</div>
     <input type="text" class="form-control" placeholder="Input group example" aria-label="Input group example" aria-describedby="btnGroupAddon">
     </div>
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <form action="Home.php" method="get">
    <button type="button" class="btn btn-link">Home</button>
-   <button type="button" class="btn btn-link">O que é o autismo?</button>
-   <button type="button" class="btn btn-link">Quem somos?</button>
-   <button type="button" class="btn btn-link">Nosso trabalho</button>
+   </form>
    </div>
 </body>
 </html>
