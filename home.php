2653a60af6293670457b863cdc81b3ebfcb253e3
Renato682 <renatoarouche3@gmail.com>
2025-05-30 23:29:42 -0300

    Update home.php

--- home.php
+++ home.php
@@ -1,20 +1,41 @@
+<?php
+session_start();
+include("header.php");
+include("home.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="projeto.css">
-    <title>Document</title>
+    <title>Espectro</title>
     <link rel="stylesheet" href="npm i bootstrap/@5.3.6">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
 </head>
-<body>
-    <h1>
-        O que é o autismo?
-    </h1>
-    <h3>
-        O Transtorno do Espectro Autista (TEA) é uma condição do neurodesenvolvimento que afeta a forma como a pessoa se comunica, interage socialmente e percebe o mundo ao seu redor. Cada pessoa dentro do espectro é única  com suas próprias habilidades, 
-        desafios, formas de se expressar e de se conectar. 
-    </h3>
+ <body>
+ </div>
+           <!-- Botão que redireciona para cadastro.php -->
+<form action="cadastro.php" method="get">
+    <button type="submit">Ir para Cadastro</button>
+</form>
+
+                <a href="#">Esqueceu a senha?</a>
+                <span>Não tem uma conta? <a href="#">Cadastre-se</a></span>
+            </div>
+        </form>
+    </div>
+  </div>
+           <!-- Botão que redireciona para cadastro.php -->
+     
+
+            </div>
+        </form>
+    </div>
+ </body>
+</html>
+
 </body>
 </html>
